80c2b2779cb29d71bb36ecf52ea1958e24efd968
Kevin Gil <keven.gs@gmail.com>
2012-06-07 07:57:07 -0700

    bugs with jQuery...

--- rec/aboutme.js
+++ rec/aboutme.js
@@ -2,6 +2,7 @@
 $("a.aboutme").click(function () {
 if ($("div.about:first").is(":hidden")) {
 $("div.about").slideDown("fast");
+$("div.about").slideUp("fast");
 } else {
 $("div.about").slideUp("fast");
 }
