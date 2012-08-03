fe2d5bce47e34bb1d1d22a81df9389403ebd6a87
Kevin Gil <keven.gs@gmail.com>
2012-08-02 22:08:23 -0700

    adding more events for popcorn poroject

--- popcorn/istupid/pops.js
+++ popcorn/istupid/pops.js
@@ -9,14 +9,53 @@
            });
             popcorn.footnote({
              start: 4,
-             end: 7,
+             end: 9,
              target: "popcont2",
-             text: "<img src=\"googleplay.png\"/>"
+             text: "<div align=\"center\">like this?<img src=\"googleplay.png\" style=\"height:200px;padding-top:5px;\"/></div>"
            });
             popcorn.footnote({
              start: 7,
-             end: 10,
+             end: 13,
              target: "popcont",
-             text: "<img src=\"bitchplease.png\" style=\"width:300px;\"/>"
+             text: "<div align=\"center\"><img src=\"bitchplease.png\" style=\"width:300px;\"/></div>"
+           });
+             popcorn.footnote({
+             start: 9,
+             end: 13,
+             target: "popcont",
+             text: "<h1>THIS TOO!!!</h1>"
+           });
+             popcorn.footnote({
+             start: 9,
+             end: 13,
+             target: "popcont2",
+             text: "<div align=\"center\"><img src=\"amazon-appstore.png\" style=\"height:200px;\"/></div>"
+           });
+             popcorn.footnote({
+             start: 13,
+             end: 20,
+             target: "popcont2",
+             text: "<h1>REALLY?????</h1>"
+           });
+             popcorn.footnote({
+             start: 14,
+             end: 20,
+             target: "popcont2",
+             text: "<h1>Can you do it remotely??? :P</h1><br><h1>from computer w/o wires?</h1>"
+           });
+             popcorn.footnote({
+             start: 14,
+             end: 20,
+             target: "popcont",
+             text: "<div align=\"center\"><img src=\"remote-dl.png\"/></div>"
            });
          }, false )
+
+/*
+           popcorn.footnote({
+             start: ,
+             end: ,
+             target: "popcont",
+             text: ""
+           });
+*/           
