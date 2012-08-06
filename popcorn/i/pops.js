e9c88f99f60c388e140dc3dfaf4c07c689196737
Kevin Gil <keven.gs@gmail.com>
2012-08-06 15:36:41 -0700

    cahnged URL and added more events/pics

---
+++ popcorn/i/pops.js
@@ -0,0 +1,104 @@
+         document.addEventListener( "DOMContentLoaded", function() {
+           var popcorn = Popcorn( "#video" );
+ 
+           popcorn.footnote({
+             start: 0.5,
+             end: 3.5,
+             target: "popcont",
+             text: "<div align=\"center\"><h1>ANDROID ftw!!!</h1></div>"
+           });
+            popcorn.footnote({
+             start: 4,
+             end: 9,
+             target: "popcont2",
+             text: "<div align=\"center\"><p>like this?</p></div>"
+           });
+            popcorn.footnote({
+             start: 4,
+             end: 9,
+             target: "popcont2",
+             text: "<div align=\"center\"><img src=\"googleplay.png\" style=\"height:190px;padding-top:0px;\"/></div>"
+           });
+            popcorn.footnote({
+             start: 7,
+             end: 13,
+             target: "popcont",
+             text: "<div align=\"center\"><img src=\"bitchplease.png\" style=\"width:300px;\"/></div>"
+           });
+             popcorn.footnote({
+             start: 9,
+             end: 13,
+             target: "popcont",
+             text: "<div align=\"center\"><h1>THIS TOO!!!</h1></div>"
+           });
+             popcorn.footnote({
+             start: 9,
+             end: 13,
+             target: "popcont2",
+             text: "<div align=\"center\"><img src=\"amazon-appstore.png\" style=\"height:200px;padding-top:10px;\"/></div>"
+           });
+             popcorn.footnote({
+             start: 13,
+             end: 20,
+             target: "popcont2",
+             text: "<div align=\"center\" style=\"padding-top:10px;\" ><h1>REALLY????? -____-</h1></div>"
+           });
+             popcorn.footnote({
+             start: 14,
+             end: 20,
+             target: "popcont2",
+             text: "<div align=\"center\"><h1>Can you do it remotely??? :P<br>from computer w/o wires?</h1></div>"
+           });
+             popcorn.footnote({
+             start: 14,
+             end: 20,
+             target: "popcont",
+             text: "<div align=\"center\"><img src=\"remote-dl.png\" style=\"padding-top:20px;\"/></div>"
+           });
+             popcorn.footnote({
+             start: 21,
+             end: 27,
+             target: "popcont",
+             text: "<div align=\"center\"><img src=\"starcbucks-ss.png\" style=\"padding-top:10px;\"/></div>"
+           });
+             popcorn.footnote({
+             start: 21,
+             end: 27,
+             target: "popcont2",
+             text: "<div align=\"center\" style=\"padding-top:30px;\"><h1>This on my Galaxy Nexus<br>-----></h1></div>"
+           });
+             popcorn.footnote({
+             start: 29,
+             end: 32,
+             target: "popcont",
+             text: "<div align=\"center\" style=\"padding-top:30px;\"><h1>What</h1></div>"
+           });
+             popcorn.footnote({
+             start: 29.3,
+             end: 32,
+             target: "popcont",
+             text: "<div align=\"center\" style=\"padding-top:30px;\"><h1>the</h1></div>"
+           });
+             popcorn.footnote({
+             start: 29.6,
+             end: 32,
+             target: "popcont2",
+             text: "<div align=\"center\" style=\"padding-top:50px;\"><p style=\"font-size:50px;\">!@#$% !!!!</p></div>"
+           });
+             popcorn.footnote({
+             start: 29.6,
+             end: 32,
+             target: "popcont",
+             text: "<div align=\"center\" style=\"padding-top:30px;\"><img src=\"fffuuu.png\" style=\"height:250px;\"/></div>"
+           });
+
+         }, false )
+
+/*
+           popcorn.footnote({
+             start: ,
+             end: ,
+             target: "popcont",
+             text: ""
+           });
+*/           
