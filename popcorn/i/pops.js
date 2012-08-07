3fa68d63a250cb0aa795e92783697152e8bf5a70
Kevin Gil <keven.gs@gmail.com>
2012-08-06 21:44:59 -0700

    added more pops and images till min 1:33

--- popcorn/i/pops.js
+++ popcorn/i/pops.js
@@ -41,7 +41,7 @@
              start: 9,
              end: 13,
              target: "popcont",
-             text: "<div align=\"center\"><h1>THIS TOO!!!</h1></div>"
+             text: "<div align=\"center\"><br><br><h1>THIS TOO!!!</h1></div>"
            });
              popcorn.footnote({
              start: 9,
@@ -81,13 +81,13 @@
            });
              popcorn.footnote({
              start: 23,
-             end: 28,
+             end: 29,
              target: "popcont",
              text: "<div align=\"center\" style=\"padding-top:30px;\"><img src=\"android-beam-pic.png\" style=\"height:170px;\"/><br><img src=\"android-beam.png\" style=\"height:120px;\"/><img src=\"nfc-googlewallet.png\" style=\"height:190px;\"/></div>"
            });
              popcorn.footnote({
              start: 23,
-             end: 28,
+             end: 29,
              target: "popcont2",
              text: "<div align=\"center\" style=\"padding-top:30px;\"><h1>Why u no haz NFC iphone?</h1><div align=\"center\"><img src=\"troll.png\" style=\"height:120px;\"/><img src=\"nfc-logo.png\" style=\"height:120px;\"/></div></div>"
            });
@@ -115,7 +115,174 @@
              target: "popcont",
              text: "<div align=\"center\" style=\"padding-top:30px;\"><img src=\"fffuuu.png\" style=\"height:250px;\"/></div>"
            });
-
+             popcorn.footnote({
+             start: 32.3,
+             end: 45,
+             target: "popcont",
+             text: "<div align=\"center\" style=\"padding-top:2px;\"><h1>Zero shooter lag</h1><img src=\"galaxy-nexus-camera.png\" style=\"height:225px;\"/></div>"
+           });
+             popcorn.footnote({
+             start: 35.4,
+             end: 45,
+             target: "popcont",
+             text: "<div align=\"center\" style=\"padding-top:2px;\"><h1>41 Megapixels!</h1><img src=\"41-mp-nokia.png\" style=\"height:220px;\"/></div>"
+           });
+             popcorn.footnote({
+             start: 39,
+             end: 45,
+             target: "popcont2",
+             text: "<div align=\"center\" ><br><br><a href=\"http://www.phonearena.com/news/Samsung-Galaxy-Nexus-vs-Apple-iPhone-4S-camera-comparison_id24078\" style=\"text-decoration:underline; padding-top:30px;\"><h1>Galaxy Nexus (5MP)<br>vs<br>iPhone 4S (8MP)</h1><h2>Galaxy Nexus wins ;)</h2></a></div>"
+           });
+             popcorn.footnote({
+             start: 45,
+             end: 47,
+             target: "popcont",
+             text: "<div align=\"center\" style=\"padding-top:2px;\"><img src=\"photo-edit-1.png\" style=\"width:290px;\"/></div>"
+           });
+             popcorn.footnote({
+             start: 45,
+             end: 47,
+             target: "popcont2",
+             text: "<div align=\"center\"><br><br><br><br><br><h1>Light Effects</h1></div>"
+           });
+             popcorn.footnote({
+             start: 47,
+             end: 49,
+             target: "popcont",
+             text: "<div align=\"center\" style=\"padding-top:2px;\"><img src=\"photo-edit-4.png\" style=\"width:290px;\"/></div>"
+           });
+             popcorn.footnote({
+             start: 47,
+             end: 49,
+             target: "popcont2",
+             text: "<div align=\"center\"><br><br><br><br><br><h1>Crop - Red Eye - Face Glow - Face Tan</h1></div>"
+           });
+             popcorn.footnote({
+             start: 49,
+             end: 51,
+             target: "popcont",
+             text: "<div align=\"center\" style=\"padding-top:2px;\"><img src=\"photo-edit-4.png\" style=\"width:290px;\"/></div>"
+           });
+             popcorn.footnote({
+             start: 49,
+             end: 51,
+             target: "popcont2",
+             text: "<div align=\"center\"><br><br><br><br><br><h1>Crop - Red Eye - Face Glow - Face Tan</h1></div>"
+           });
+             popcorn.footnote({
+             start: 51,
+             end: 56,
+             target: "popcont",
+             text: "<div align=\"center\" style=\"padding-top:2px;\"><img src=\"share-ev1.png\" style=\"width:290px;\"/></div>"
+           });
+             popcorn.footnote({
+             start: 56,
+             end: 60,
+             target: "popcont",
+             text: "<div align=\"center\" style=\"padding-top:2px;\"><img src=\"share-ev2.png\" style=\"width:290px;\"/></div>"
+           });
+             popcorn.footnote({
+             start: 51,
+             end: 60,
+             target: "popcont2",
+             text: "<div align=\"center\"><br><br><br><br><h1>BUilt in<br>Share EVERYWHERE!</h1></div>"
+           });
+             popcorn.footnote({
+             start: 60,
+             end: 63,
+             target: "popcont",
+             text: "<div align=\"center\" style=\"padding-top:2px;\"><img src=\"photo-edit-3.png\" style=\"width:290px;\"/></div>"
+           });
+             popcorn.footnote({
+             start: 60,
+             end: 63,
+             target: "popcont2",
+             text: "<div align=\"center\"><br><br><br><br><br><h1>Pixel Effects</h1></div>"
+           });
+             popcorn.footnote({
+             start: 63,
+             end: 65,
+             target: "popcont",
+             text: "<div align=\"center\" style=\"padding-top:2px;\"><img src=\"photo-edit-3.png\" style=\"width:290px;\"/></div>"
+           });
+             popcorn.footnote({
+             start: 63,
+             end: 65,
+             target: "popcont2",
+             text: "<div align=\"center\"><br><br><br><br><br><h1>Color Effects</h1></div>"
+           });
+             popcorn.footnote({
+             start: 65,
+             end: 68,
+             target: "popcont",
+             text: "<div align=\"center\" style=\"padding-top:2px;\"><img src=\"music.png\" style=\"width:290px;\"/></div>"
+           });
+             popcorn.footnote({
+             start: 65,
+             end: 68,
+             target: "popcont2",
+             text: "<div align=\"center\"><br><br><br><br><br><h1>Still just a music player,<br>I use <b>Google Music<b></h1></div>"
+           });
+             popcorn.footnote({
+             start: 68,
+             end: 70,
+             target: "popcont",
+             text: "<div align=\"center\" style=\"padding-top:2px;\"><img src=\"my-music.png\" style=\"width:290px;\"/></div>"
+           });
+             popcorn.footnote({
+             start: 68,
+             end: 70,
+             target: "popcont2",
+             text: "<div align=\"center\"><br><br><br><br><br><h1>Google Music<br> has my music everywhere for free</h1></div>"
+           });
+             popcorn.footnote({
+             start: 70,
+             end: 74,
+             target: "popcont",
+             text: "<div align=\"center\" style=\"padding-top:2px;\"><img src=\"my-playlist.png\" style=\"width:290px;\"/></div>"
+           });
+             popcorn.footnote({
+             start: 70,
+             end: 74,
+             target: "popcont2",
+             text: "<div align=\"center\"><br><br><br><br><br><h1>It has all my playlist =D</h1></div>"
+           });
+             popcorn.footnote({
+             start: 74,
+             end: 80,
+             target: "popcont",
+             text: "<div align=\"center\" style=\"padding-top:2px;\"><h1>Google Music</h1><img src=\"google-play-music.png\" style=\"width:290px;\"/><h1>Amazon MP3</h1><img src=\"amazon-mp3.png\" style=\"width:290px;\"/><h1>Anywhere on the internet</h1><img src=\"internet-globe.png\" style=\"height:85px;\"/></div>"
+           });
+             popcorn.footnote({
+             start: 74,
+             end: 80,
+             target: "popcont2",
+             text: "<div align=\"center\"><br><br><br><br><br><h1>I can download from here:</h1></div>"
+           });
+             popcorn.footnote({
+             start: 80,
+             end: 86,
+             target: "popcont2",
+             text: "<div align=\"center\" style=\"padding-top:2px;\"><img src=\"google-play-recomends.png\" style=\"height:200px;\"/></div>"
+           });
+             popcorn.footnote({
+             start: 80,
+             end: 86,
+             target: "popcont",
+             text: "<div align=\"center\"><br><h1>Mine<br>recomends<br>too<br><img src=\"troll.png\" style=\"width:290px;\"/></h1></div>"
+           });
+             popcorn.footnote({
+             start: 86,
+             end: 93,
+             target: "popcont",
+             text: "<div align=\"center\" style=\"padding-top:2px;\"><h1>Google Music</h1><img src=\"music-synced.png\" style=\"width:275px;\"/></div>"
+           });
+             popcorn.footnote({
+             start: 86,
+             end: 93,
+             target: "popcont2",
+             text: "<div align=\"center\"><br><br><br><br><br><h1>Btw, it syncs everything automatically<br>FOR FREE!</h1></div>"
+           });
          }, false )
 
 /*
